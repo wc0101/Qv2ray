860234e1d35f7e02489fbf49164f2420bbc3a72a
ymshenyu <ymshenyu@gmail.com>
2020-04-30 15:58:23 +0800

    kill process before uninstall/install

--- cmake/deployment.cmake
+++ cmake/deployment.cmake
@@ -43,6 +43,10 @@ if(MSVC)
         set(CPACK_NSIS_MUI_UNIICON "${CMAKE_SOURCE_DIR}/assets/icons/qv2ray.ico")
         set(CPACK_NSIS_DISPLAY_NAME "Qv2ray")
         set(CPACK_NSIS_PACKAGE_NAME "Qv2ray")
+        set(CPACK_NSIS_EXTRA_PREINSTALL_COMMANDS "
+            StrCpy $0 "qv2ray.exe"
+            KillProc::KillProcesses
+        ")
         set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
             CreateShortCut \\\"$DESKTOP\\\\Qv2ray.lnk\\\" \\\"$INSTDIR\\\\qv2ray.exe\\\"
             CreateDirectory \\\"$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\Qv2ray\\\"
@@ -54,6 +58,8 @@ if(MSVC)
             WriteRegStr HKLM \\\"Software\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Uninstall\\\\qv2ray\\\" \\\"URLInfoAbout\\\" \\\"https://github.com/Qv2ray/Qv2ray\\\"
         ")
         set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "
+            StrCpy $0 "qv2ray.exe"
+            KillProc::KillProcesses
             Delete \\\"$DESKTOP\\\\Qv2ray.lnk\\\"
             Delete \\\"$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\Qv2ray\\\\Qv2ray.lnk\\\"
             RMDir \\\"$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\Qv2ray\\\"
